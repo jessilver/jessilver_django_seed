1f39dd179cda3345821ab74575c273a5dcab1da2
Jesse Silva <jesse1eliseu@gmail.com>
2025-03-15 20:02:49 -0300

    Refactor Django seed package structure and implement base seeder class

--- setup.py
+++ setup.py
@@ -1,8 +1,8 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='django_seed',
-    version='0.1.0',
+    name='jessilver_django_seed',
+    version='0.1.1',
     packages=find_packages(),
     include_package_data=True,
     license='MIT License',
