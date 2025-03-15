c73e6d9fe606d95d5b2963f1292183e46f715684
Jesse Silva <jesse1eliseu@gmail.com>
2025-03-15 20:44:49 -0300

    Rename app from django_seed to jessilver_django_seed in apps.py

--- jessilver_django_seed/apps.py
+++ jessilver_django_seed/apps.py
@@ -3,4 +3,4 @@ from django.apps import AppConfig
 
 class DjangoSeedConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'django_seed'
+    name = 'jessilver_django_seed'
