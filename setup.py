42155836781f63a391d9e2c8238079137e319e1a
Jesse Silva <jesse1eliseu@gmail.com>
2025-03-15 21:01:55 -0300

    Bump version to 1.0.2 and add management command for database seeding

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='jessilver_django_seed',
-    version='1.0.1',
+    version='1.0.2',
     packages=find_packages(),
     include_package_data=True,
     license='MIT License',
